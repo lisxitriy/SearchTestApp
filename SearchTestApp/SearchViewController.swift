9bd6162af94f727ce9c1c4cd7cf94836ceb702af
author <email>
2021-04-19 23:21:41 +0300

    Настройка кастомных ячеек для результатов поиска

--- SearchTestApp/SearchViewController.swift
+++ SearchTestApp/SearchViewController.swift
@@ -15,11 +15,25 @@ class SearchViewController: UIViewController {
     var searchResults = [SearchResult]()
     var hasSearched = false
     
+    struct TableView {
+      struct CellIdentifiers {
+        static let searchResultCell = "SearchResultCell"
+        static let nothingFoundCell = "NothingFoundCell"
+      }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         tableView.contentInset = UIEdgeInsets(top: 56, left: 0, bottom: 0, right: 0)
+        
+        var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
+        
+        cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
     }
 
 
@@ -35,15 +49,15 @@ extension SearchViewController: UISearchBarDelegate {
         
         searchResults = []
         if searchBar.text! != "justin bieber" {
-        for i in 0...2 {
+          for i in 0 ... 2 {
             let searchResult = SearchResult()
-            searchResult.name = String(format: "Fake Resalt %d for %@", i, searchBar.text!)
+            searchResult.name = String(format: "Fake Result %d for", i)
             searchResult.artistName = searchBar.text!
             searchResults.append(searchResult)
+          }
         }
         hasSearched = true
         tableView.reloadData()
-    }
     }
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
@@ -66,25 +80,18 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let cellIdentifier = "SearchResultCell"
-        
-        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-        
-        if cell == nil {
-            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-        }
-        
         if searchResults.count == 0 {
-            cell.textLabel!.text = "(No this found)"
-            cell.detailTextLabel?.text = ""
+            return tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.nothingFoundCell, for: indexPath)
         } else {
             
+            let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
         let searchResult = searchResults[indexPath.row]
-        cell.textLabel!.text = searchResult.name
-        cell.detailTextLabel!.text = searchResult.artistName
+        cell.nameLabel.text = searchResult.name
+        cell.artistNameLabel.text = searchResult.artistName
+            
+            
+            return cell
         }
-
-        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
