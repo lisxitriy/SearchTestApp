5d3b5b66f211f7a9b491ae79c5ce6b1269b6a80b
author <email>
2021-04-19 21:29:33 +0300

    добавление search bar и table view. проверка их работоспособности с фейковыми данными

---
+++ SearchTestApp/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  SearchTestApp
+//
+//  Created by Olga Trofimova on 18.04.2021.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
