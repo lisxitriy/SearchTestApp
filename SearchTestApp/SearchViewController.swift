892cb73526783ac89019cea6557ddbef90b60a5c
author <email>
2021-04-30 22:15:17 +0300

    получение данных с сайта

--- SearchTestApp/SearchViewController.swift
+++ SearchTestApp/SearchViewController.swift
@@ -28,7 +28,7 @@ class SearchViewController: UIViewController {
         
         
         tableView.contentInset = UIEdgeInsets(top: 56, left: 0, bottom: 0, right: 0)
-        
+//        
         var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
         
@@ -42,24 +42,31 @@ class SearchViewController: UIViewController {
 
 }
 
+
+
 //MARK: - Search Bar Delegate
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-//        скрыть клавиатуру по тапу на поиск
-        searchBar.resignFirstResponder()
-        
-        searchResults = []
-        if searchBar.text! != "justin bieber" {
-          for i in 0 ... 2 {
-            let searchResult = SearchResult()
-            searchResult.name = String(format: "Fake Result %d for", i)
-            searchResult.artistName = searchBar.text!
-            searchResults.append(searchResult)
-          }
+        if searchBar.text!.isEmpty {
+            //        скрыть клавиатуру по тапу на поиск
+                    searchBar.resignFirstResponder()
         }
+        
         hasSearched = true
+        searchResults = []
+        
+        let url = itunesURL(searchText: searchBar.text!)
+        print("URL: '\(url)")
+        
+        if let data = performStoreRequest(with: url) {
+            
+           searchResults = parse(data: data)
+            
+            searchResults.sort { $0 < $1 }
+        }
+        
         tableView.reloadData()
     }
     
@@ -90,8 +97,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
         let searchResult = searchResults[indexPath.row]
         cell.nameLabel.text = searchResult.name
-        cell.artistNameLabel.text = searchResult.artistName
-            
+            cell.artistNameLabel.text = String(format: "%@ (%@)", searchResult.artist, searchResult.type)
             
             return cell
         }
@@ -109,4 +115,45 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         }
     }
     
+    //MARK: - Helper Method
+    
+    func itunesURL(searchText: String) -> URL {
+        
+        let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@", encodedText)
+        let url = URL(string: urlString)
+        return url!
+    }
+    
+    func performStoreRequest(with url: URL) -> Data? {
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            print("Download Error: \(error.localizedDescription)")
+        }
+        showNetworkError()
+        return nil
+    }
+    
+    func parse(data: Data) -> [SearchResult] {
+        do {
+            let decoder = JSONDecoder()
+            
+            let result = try decoder.decode(ResultArray.self, from: data)
+            
+            return result.results
+        } catch {
+            print("JSON Error: \(error)")
+        return []
+        }
+    }
+    
+    func showNetworkError() {
+        let alert = UIAlertController(title: "Wooops...", message: "There was an error accessing the Itunes Store" + "Please try again" , preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
