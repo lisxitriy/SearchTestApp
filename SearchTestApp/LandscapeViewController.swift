d226cf4f055570e5b4e0f2fb7a4ffe31a3bb779f
author <email>
2021-05-11 22:36:59 +0300

    добавление и скачивание изображений для кнопок

--- SearchTestApp/LandscapeViewController.swift
+++ SearchTestApp/LandscapeViewController.swift
@@ -15,6 +15,7 @@ class LandscapeViewController: UIViewController {
     var searchResults = [SearchResult]()
     
     private var firstTime = true
+    private var downloads = [URLSessionDownloadTask]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,6 +49,13 @@ class LandscapeViewController: UIViewController {
             firstTime = false
             titleButtons(searchResults)
         }
+    }
+    
+    deinit {
+      print("deinit \(self)")
+      for task in downloads {
+        task.cancel()
+      }
     }
     
     //MARK: - Actions
@@ -95,9 +103,9 @@ class LandscapeViewController: UIViewController {
         var column = 0
         var x = marginX
         for (index, result) in searchResults.enumerated() {
-            let button = UIButton(type: .system)
-            button.backgroundColor = .white
-            button.setTitle("\(index)", for: .normal)
+            let button = UIButton(type: .custom)
+            button.setBackgroundImage(UIImage(named: "LandscapeButton"), for: .normal)
+            downloadImage(for: result, andPlaceOn: button)
             
             button.frame = CGRect(x: x + paddingHorz, y: marginY + CGFloat(row) * itemHeight + paddingVert, width: buttonWidth, height: buttonHeight)
             scrollView.addSubview(button)
@@ -121,6 +129,25 @@ class LandscapeViewController: UIViewController {
         pageControl.numberOfPages = numPages
         pageControl.currentPage = 0
     }
+    
+    private func downloadImage(for searchResult: SearchResult, andPlaceOn button: UIButton) {
+      if let url = URL(string: searchResult.imageSmall) {
+        let task = URLSession.shared.downloadTask(with: url) {
+          [weak button] url, _, error in
+          if error == nil, let url = url, let data = try? Data(contentsOf: url),
+            let image = UIImage(data: data) {
+            DispatchQueue.main.async {
+              if let button = button {
+                button.setImage(image, for: .normal)
+              }
+            }
+          }
+        }
+        task.resume()
+        downloads.append(task)
+      }
+    }
+
     
     /*
     // MARK: - Navigation
